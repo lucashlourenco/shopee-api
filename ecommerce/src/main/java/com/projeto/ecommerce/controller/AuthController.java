07ff86358e6a55f36f8cfee8f6980d0368757d00
Isaac Pessoa <lucasdesouza616@gmail.com>
2025-11-24 14:57:58 -0300

    Feat: Adiciona Docker Compose e ajusta conexao com banco

--- ecommerce/src/main/java/com/projeto/ecommerce/controller/AuthController.java
+++ ecommerce/src/main/java/com/projeto/ecommerce/controller/AuthController.java
@@ -1,21 +1,28 @@
 package com.projeto.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.projeto.ecommerce.dto.AuthDTO;
 import com.projeto.ecommerce.dto.LoginResponseDTO;
 import com.projeto.ecommerce.dto.RegisterDTO;
 import com.projeto.ecommerce.model.Usuario;
 import com.projeto.ecommerce.repository.UsuarioRepository;
 import com.projeto.ecommerce.service.TokenService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("auth")
 public class AuthController {
+
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -24,15 +31,17 @@ public class AuthController {
     private TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody AuthDTO data){
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.senha());
         var auth = this.authenticationManager.authenticate(usernamePassword);
+        
         var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+        
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody RegisterDTO data){
+    public ResponseEntity<Void> register(@RequestBody @Valid RegisterDTO data){
         if(this.repository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.senha());
@@ -43,6 +52,7 @@ public class AuthController {
         newUser.setTipo(data.tipo());
         
         this.repository.save(newUser);
+        
         return ResponseEntity.ok().build();
     }
 }
