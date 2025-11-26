368ac420b074bf5d1726eeb53261aa8c5314c54c
Isaac Pessoa <lucasdesouza616@gmail.com>
2025-11-26 15:24:36 -0300

    Refactor: Implementa variaveis de ambiente (.env) e segurança

--- ecommerce/src/main/java/com/projeto/ecommerce/config/SecurityConfig.java
+++ ecommerce/src/main/java/com/projeto/ecommerce/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.projeto.ecommerce.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,14 +19,14 @@ public class SecurityConfig {
     @Autowired
     SecurityFilter securityFilter;
 
-    @Bean
+   @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        
+                        .requestMatchers("/auth/**").permitAll() 
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
